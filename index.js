0afe3f8380b819f5c5746df9c5022e8bbecfdeec
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-15 19:00:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  while (0 < facts.length) {
+    var i = 0;
+    i++;
+    facts[i]= facts[i]+'!!!';
+    --facts.length;
+  }
+  return facts;
+}
